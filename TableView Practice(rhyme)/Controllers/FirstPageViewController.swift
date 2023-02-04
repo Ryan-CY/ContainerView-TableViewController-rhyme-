91ce81ad57a1948fe20bc5bcd396be0c37b93b0a
Ryan CY <phillin820109@msn.com>
2023-02-04 20:03:57 +0800

    Dynamic TableViewController

---
+++ TableView Practice(rhyme)/Controllers/FirstPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstPageViewController.swift
+//  TableView Practice(rhyme)
+//
+//  Created by Ryan Lin on 2023/2/4.
+//
+
+import UIKit
+
+class FirstPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
